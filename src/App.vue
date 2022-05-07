ab70992561b196b2ce45802d6a31e0f6f87294e0
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-05-07 12:54:07 +0200

    Index: creato scaffholding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-grid/>
+  </div>
+</template>
+
+<script>
+import AppGrid from './components/AppGrid.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppGrid
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
