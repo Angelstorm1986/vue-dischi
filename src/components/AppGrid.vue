e5c8db6689a617dd409ce91105ae7c711bf62f99
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-05-09 19:29:48 +0200

    Index: completamento

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -1,8 +1,9 @@
 <template>
   <section class="container">
     <app-loader v-show="loading"/>
+    <app-search @performSearch="mySearch" :diskAll="diskList" :diskGenres="genre"/>
     <div class="disk-container row row-cols-5">
-      <div class="col gy-3" v-for="(disk, index) in diskList" :key="index" >
+      <div class="col gy-3" v-for="(disk, index) in filteredList" :key="index" >
         <app-disk :author="disk.author" :genre="disk.genre" :poster="disk.poster" :title="disk.title" :year="disk.year"/>
       </div>
     </div>
@@ -12,26 +13,51 @@
 <script>
 
 import axios from 'axios';
+import AppSearch from './AppSearch.vue';
 import AppLoader from './AppLoader.vue';
 import AppDisk from './AppDisk.vue';
 
 export default {
   name: 'AppGrid',
   components: {
+    AppSearch,
     AppLoader,
     AppDisk
   },
   data(){
     return {
       diskList: [],
-      loading: false
+      loading: false,
+      searchText: '',
+      genre: []
+    }
+  },
+  methods:{
+    mySearch(text){
+      this.searchText = text;
+    }  
+  },
+  computed:{
+    filteredList(){
+      if(this.searchText === '') {
+        return this.diskList
+      }else {
+        return this.diskList.filter((el) => el.genre === this.searchText)
+      }
     }
   },
   mounted() {
     this.loading = true;
     setTimeout(()=>{
       axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res)=>{
+        console.log(res)
         this.diskList = res.data.response;
+        this.diskList.forEach((el) => {
+          if(!this.genre.includes(el.genre)) {
+            this.genre.push(el.genre)
+          }
+        })
+        console.log(this.genre)
         this.loading = false;
       }).catch((error) => {
         this.loading = false;
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <select name="genre" id="genre" v-model="inputText" @change="changeSearch">
+            <option value="">Seleziona il genere che preferisci</option>
+            <option :value="genre" v-for="genre in diskGenres" :key="genre">{{genre}}</option>
+        </select>
+    </div>  
+</template>
+
+<script>
+export default {
+    name:'AppSearch',
+    data(){
+        return{
+            inputText:'',
+        }
+    },
+    props:['diskGenres'],
+    methods:{  
+        changeSearch(){
+            this.$emit('performSearch', this.inputText)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
